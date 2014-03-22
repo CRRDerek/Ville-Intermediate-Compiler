b9e662d4e2730b3d0cf837bab1e8f597f0e94f17
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2014-03-22 06:15:57 -0400

    Added Project

---
+++ CCTagIntermediateCompiler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMFParser;
+
+namespace CCTagIntermediateCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = args[0];
+            VMF vmf = new VMF(File.ReadAllLines(fileName));
+            if (TagModifications(vmf))
+                File.WriteAllLines(fileName, vmf.ToVMFStrings());
+        }
+
+        internal static bool TagModifications(VMF vmf)
+        {
+            bool hasChanged = false;
+
+            return hasChanged;
+        }
+
+        internal static bool Mod_GreenFizzlers(VMF vmf)
+        {
+
+            return false;
+        }
+    }
+}
