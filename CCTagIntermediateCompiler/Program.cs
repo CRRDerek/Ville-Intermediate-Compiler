8bb36ece7a6d2b7673e08bb45ea79185774ba892
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2014-05-18 12:52:31 -0400

    Paint in map always set to 1

--- CCTagIntermediateCompiler/Program.cs
+++ CCTagIntermediateCompiler/Program.cs
@@ -12,7 +12,7 @@ namespace CCTagIntermediateCompiler
     {
         static void Main(string[] args)
         {
-            string fileName = args[0];
+            string fileName = args.First();
             VMF vmf = new VMF(File.ReadAllLines(fileName));
             if (TagModifications(vmf))
                 File.WriteAllLines(fileName, vmf.ToVMFStrings());
@@ -21,11 +21,32 @@ namespace CCTagIntermediateCompiler
         internal static bool TagModifications(VMF vmf)
         {
             bool hasChanged = false;
+            hasChanged = Mod_GreenFizzlerFlag(vmf) | hasChanged;
+            hasChanged = Mod_EnablePaintInMap(vmf) | hasChanged;
+            return hasChanged;
+        }
+
+        internal static bool Mod_EnablePaintInMap(VMF vmf)
+        {
+            bool hasChanged = false;
+
+            VBlock world = vmf.Body.Where(item => item.Name == "world").First() as VBlock;
+            VProperty paintInMap = world.Body.Where(item => item.Name == "paintinmap").First() as VProperty;
+            if (paintInMap == null)
+            {
+                world.Body.Add(new VProperty("paintinmap", "1"));
+                hasChanged = true;
+            }
+            else if (paintInMap.Value != "1")
+            {
+                paintInMap.Value = "1";
+                hasChanged = true;
+            }
 
             return hasChanged;
         }
 
-        internal static bool Mod_GreenFizzlers(VMF vmf)
+        internal static bool Mod_GreenFizzlerFlag(VMF vmf)
         {
 
             return false;
